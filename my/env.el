fe3357f5adc06e00195650487b4e3ebd6e759476
Steve Lianoglou <slianoglou@gmail.com>
2011-05-14 17:00:29 -0400

    Ensure R starts in 64-bit mode

--- my/env.el
+++ my/env.el
@@ -2,6 +2,10 @@
 
 (setq exec-path (cons "/usr/local/bin" exec-path))
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+
+;; Ensure R starts in 64bit mode
+(setenv "R_ARCH" "/x86_64")
+
 ;; (setq exec-path (cons "/usr/texbin" exec-path))
 ;; (setenv "PATH" (concat "/usr/texbin:" (getenv "PATH")))
 ;; (setq exec-path (cons "/usr/local/share/npm/bin" exec-path))
