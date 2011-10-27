e4d5c9df1a049058faa6faeb02a05410fa7c3740
Steve Lianoglou <slianoglou@gmail.com>
2011-10-27 11:42:02 -0400

    Open ibuffer with point at current buffer in the list.

--- my/ibuffer.el
+++ my/ibuffer.el
@@ -40,4 +40,14 @@
   (lambda ()
     (ibuffer-switch-to-saved-filter-groups "default")))
 
+;; Ensure ibuffer opens with point at the current buffer's entry.
+;; http://stackoverflow.com/questions/3417438
+(defadvice ibuffer
+  (around ibuffer-point-to-most-recent) ()
+  "Open ibuffer with cursor pointed to most recent buffer name."
+  (let ((recent-buffer-name (buffer-name)))
+    ad-do-it
+    (ibuffer-jump-to-buffer recent-buffer-name)))
+(ad-activate 'ibuffer)
+
 ;; (provide 'my-ibuffer)
